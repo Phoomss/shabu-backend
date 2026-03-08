39bd7be07e6b89dc9190d123154dbb0c595a7d00
Phoomss <narongsak5az@gmail.com>
2026-03-09 01:54:43 +0700

    created resource use, starting now not complated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoleModule } from './role/role.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, RoleModule],
+  imports: [PrismaModule, RoleModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
